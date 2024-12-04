216273521b863215ab7898ced1aa57616507428a
heisob <heinrich@soebke.de>
2024-12-04 08:17:50 +0100

    Neues simples projekt

---
+++ mavena1/src/main/java/mavena1/MainClass1.java
@@ -0,0 +1,12 @@
+package mavena1;
+
+public class MainClass1 {
+	public static void main(String[] args) throws Exception {
+		System.out.println("MainClass1: Hello World");		
+	}
+	
+	public static int summe(int summand1, int summand2) {
+		return summand1 + summand2;
+	}
+
+}
---
+++ mavena1/src/test/java/mavena1/MainClass1Test.java
@@ -0,0 +1,14 @@
+package mavena1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainClass1Test {
+
+	@Test
+	void testSumme() {
+		assertEquals(11, mavena1.MainClass1.summe(3, 8));
+	}
+
+}
